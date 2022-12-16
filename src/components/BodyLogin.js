78c938913ba7a763e99123455354b7e7e8c7d08b
Iago Wesley Irineu Costa <iago.costa@fitbank.com.br>
2022-12-16 15:13:25 -0300

    validation login

--- src/components/BodyLogin.js
+++ src/components/BodyLogin.js
@@ -2,37 +2,50 @@ import './BodyLogin.min.css'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
 
 
 function BodyLogin() {
+    const schema = yup.object({
+        email: yup.string().required("Campo obrigatório").email("Email inválido"),
+        password: yup.string().required("Campo obrigatório").min(6, "Mínimo 6 caracteres"),
+    })
+
+    const { handleSubmit, register, formState: {errors} } = useForm({
+        resolver: yupResolver(schema)
+    }) 
+
+    const save = (data) => {
+        console.log(data)
+    }
     return ( 
+        
         <div className="body-login">
             <div className="caixa-login">
+            <form onSubmit={handleSubmit(save)}>
                 <h2>Bem vindo</h2>
-                <p>Entre na sua conta</p>
+                <p className='entre-na-conta'>Entre na sua conta</p>
                 
                 <div className="inputs">
-                <Box
-                component="form"
-                sx={{
-                    '& > :not(style)': { m: 1, width: '90%' },
-                }}
-                noValidate
-                autoComplete="off"
-                >
-                <TextField id="outlined-basic" label="Email" variant="outlined" />
-                <TextField id="outlined-password-input" label="Senha" type="password"autoComplete="current-password"
-                sx={{
-                    '& > :not(style)': {mt: 1},
-                }}
-                />
+                <Box className='box-inputs'>
+                <TextField label="Email" {...register("email")}/>
+                {errors?.email && <p className='error'>{errors?.email?.message}</p>}
+                <TextField style={{marginTop:25}} label="Senha" type="password" {...register("password")}/>
+                {errors?.password &&  
+                <p className='error'>
+                    {errors?.password?.message}
+                </p>}
                 </Box>
-                <button className='btn-entrar'>Entrar</button>
+                <button className='btn-entrar' type="submit">Entrar</button>
                 <div className="cadastre">
                 <p>Não possui conta? <Link to="/cadastro">Cadastre-se</Link></p>
                 <Link to="/esqueceusenha">Esqueceu a senha?</Link>
                 </div>
                 </div>
+                
+            </form>
                 
             </div>
         </div>
