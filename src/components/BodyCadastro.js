27fa49d25b1a9aacdbb938391c6ecb43f8add4f8
iagowesleey <iagowesley234@gmail.com>
2022-12-17 13:26:37 -0300

    validações

--- src/components/BodyCadastro.js
+++ src/components/BodyCadastro.js
@@ -1,38 +1,55 @@
-import './BodyLogin.min.css'
+import './Cadastro.min.css'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
 
 
 function BodyCadastro() {
+
+    const schema = yup.object({
+        name_prop: yup.string().required("Campo obrigatório"),
+        name_company: yup.string().required("Campo obrigatório"),
+        cnpj: yup.string().required("Campo obrigatório").min(6, "Mínimo 10 caracteres")
+    })
+
+    const { handleSubmit, register, formState: {errors} } = useForm({
+        resolver: yupResolver(schema)
+    }) 
+
+    const save = (data) => {
+        console.log(data)
+    }
+
     return ( 
         <div className="body-login">
             <div className="caixa-login">
+            <form onSubmit={handleSubmit(save)}>
+
                 <h2>Etapa 1 - Dados</h2>
                 <p>cadastre sua conta</p>
                 
                 <div className="inputs">
-                <Box
-                component="form"
-                sx={{
-                    '& > :not(style)': { m: 1, width: '90%' },
-                }}
-                noValidate
-                autoComplete="off"
-                >
-                <TextField id="outlined-basic" label="Nome do proponente" variant="outlined" />
-                <TextField id="outlined-basic" label="Nome da empresa" variant="outlined" />
-                <TextField id="outlined-basic" label="CNPJ" variant="outlined" />
+                <Box className='box-inputs'>
+                <TextField label="Nome do proponente" {...register("name_prop")}/>
+                {errors?.name_prop && <p className='error'>{errors?.name_prop?.message}</p>}
+                <TextField style={{marginTop:25}} label="Nome da empresa" {...register("name_company")}/>
+                {errors?.name_company && <p className='error'>{errors?.name_company?.message}</p>}
+                <TextField style={{marginTop:25}} label="CNPJ" {...register("cnpj")}/>
+                {errors?.cnpj && <p className='error'>{errors?.cnpj?.message}</p>}
+
     
                 </Box>
-                <button className='btn-entrar'>AVANÇAR</button>
+                <button className='btn-avancar' type="submit">AVANÇAR</button>
                 <div className="cadastre">
                 <p>Já possui uma conta? <Link to="/login">Login</Link></p>
 
                 </div>
 
                 </div>
-                
+                </form>
             </div>
         </div>
      );
--- src/components/EsqueceuSenha.js
+++ src/components/EsqueceuSenha.js
@@ -1,37 +1,46 @@
-import "./BodyLogin.min.css";
+import "./EsqueceuSenha.min.css";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
 
 function BodyCadastro() {
+
+  const schema = yup.object({
+    email: yup.string().required("Campo obrigatório").email("Email inválido"),
+})
+
+const { handleSubmit, register, formState: {errors} } = useForm({
+    resolver: yupResolver(schema)
+}) 
+
+const save = (data) => {
+    console.log(data)
+}
+
   return (
     <div className="body-login">
       <div className="caixa-login">
+        <form onSubmit={handleSubmit(save)}>
         <h2>Etapa 1 - Dados</h2>
         <p>Recupere sua conta</p>
 
         <div className="inputs">
-          <Box
-            component="form"
-            sx={{
-              "& > :not(style)": { m: 1, width: "90%" },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <TextField
-              id="outlined-basic"
-              label="Nome da empresa"
-              variant="outlined"
-            />
+          <Box className='box-inputs'>
+          <TextField label="Email" {...register("email")}/>
+          {errors?.email && <p className='error'>{errors?.email?.message}</p>}
+
           </Box>
-          <button className="btn-entrar">RECUPERAR SENHA</button>
+          <button className="btn-recuperar" type="submit">RECUPERAR SENHA</button>
           <div className="cadastre">
             <p>
               Não possui conta? <Link to="/cadastro">Cadastre-se</Link>
             </p>
           </div>
         </div>
+        </form>
       </div>
     </div>
   );
